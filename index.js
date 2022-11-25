dd536868e66bd826795382e4f7fc06a86242f205
Eman Salah <emisalah2000@gmail.com>
2022-11-25 04:43:20 +0200

    add js and getTotal function

---
+++ index.js
@@ -0,0 +1,49 @@
+// calh from id in html
+
+let title = document.getElementById('title')
+let price = document.getElementById('price')
+let taxes = document.getElementById('taxes')
+let ads = document.getElementById('ads')
+let discount = document.getElementById('discount')
+let total = document.getElementById('total')
+let count = document.getElementById('count')
+let category = document.getElementById('category')
+let submit = document.getElementById('submit')
+
+// console.log(title , price , taxes ,ads ,discount , total ,count , category , submit)
+
+
+//  function to calculate the price (get total )
+function getTotal()
+{
+    if(price.value !='')
+    {
+        let result= (+price.value + +taxes.value + +ads.value) 
+        - +discount.value;
+
+        total.innerHTML =result;
+        total.style.background= "#177e21";
+    }
+    else{
+        total.innerHTML='';
+        total.style.background="#632c51";
+    }
+}
+
+// function to create product
+
+// save data to localstorge
+
+// clear data after create
+
+// read product
+
+// count of products
+
+//delete of product
+
+//update product
+
+//search
+
+//clean input data
