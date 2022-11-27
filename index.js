adedbf008791fe3c7b8cf965b25ee9e4568514bb
Eman Salah <emisalah2000@gmail.com>
2022-11-27 16:42:16 +0200

    add delete , delete all ,update functions

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let count = document.getElementById('count')
 let category = document.getElementById('category')
 let submit = document.getElementById('submit')
 
+let mood= 'create';
+let temp;
 // console.log(title , price , taxes ,ads ,discount , total ,count , category , submit)
 
 
@@ -54,8 +56,30 @@ let new_product={
     count :count.value,
     category: category.value,
 }
-data_product.push(new_product);
+// count of products
+if  (mood ==='create')
+{
+    if(new_product.count>1)
+{
+    for (let i=0 ;i<new_product.count ;i++)
+    {
+        data_product.push(new_product);
+        
+    }
+}
+else{
+    data_product.push(new_product);
+}
+}else{
+    data_product[temp]=new_product;
+    mood='create';
+    submit.innerHTML='Create';
+    count.style.display='block';
+}
+
 localStorage.setItem('product',  JSON.stringify(data_product) )
+
+
 // console.log(data_product);
 
 cleardata()
@@ -96,21 +120,63 @@ function showData()
     <td>${data_product[i].discount}</td>
     <td>${data_product[i].total}</td>
     <td>${data_product[i].category}</td>
-    <td><button id="update">update</button></td>
-    <td><button id="delete">delete</button></td>
+    <td><button onclick="update_data(${i})" id="update">update</button></td>
+    <td><button onclick="delete_data(${i})" id="delete">delete</button></td>
 </tr>
 </tr>`;
 
  }
  document.getElementById('tbody').innerHTML=table;
+let btndelete=document.getElementById('deleteALL');
+ if(data_product.length>0)
+ {
+   btndelete.innerHTML =`<button onclick='deleteALL()'>DELETE ALL (${data_product.length})</button>`
+ }
+ else{
+    btndelete.innerHTML =''
+ }
+ getTotal();
 }
 
 showData()
-// count of products
 
 //delete of product
+function delete_data(i){
+data_product.splice(i,1);
+localStorage.product =JSON.stringify(data_product);
+showData();
+}
+
+//delete All
+function deleteALL()
+{
+    data_product.splice(0);
+    localStorage.product=JSON.stringify(data_product);
+    showData();
+}
+
+
 
 //update product
+function update_data(i)
+{
+    title.value=data_product[i].title;
+    price.value=data_product[i].price;
+    taxes.value=data_product[i].taxes;
+    ads.value=data_product[i].ads;
+    discount.value=data_product[i].discount;
+    getTotal()
+    count.style.display='none';
+    category.value=data_product[i].category;
+    submit.innerHTML='Update';
+    mood='Update'
+    temp=i;
+    scroll({
+        top:0,
+        behavior:"smooth"
+    })
+    
+}
 
 //search
 
